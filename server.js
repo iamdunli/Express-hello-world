f011db6e216e1cbad89af7bc64df09d1ef2e2b45
dunli <iamdunli@gmail.com>
2020-04-20 14:33:24 -0400

    application middleware code testing

--- server.js
+++ server.js
@@ -3,8 +3,35 @@ const express = require('express');
 const port = 1000;
 const app = express();
 
+const middleware1 = (req, res, next)=>{
+    console.log(`${req.method} : ${req.url}`);
+    next();
+}
+app.use(middleware1);
+app.use( (req, res, next)=>{
+    if (req.query.key == '123456'){
+        next();
+    }    
+    else{
+        res.status(401).send({msg : 'not authorized'})
+    }
+})
+
+app.get('/accounts', (req, res, next)=>{
+    console.log('accounts inline middleware');
+    next(new Error('oopps accounts page unable to use'))
+}, (req, res)=>{    
+    res.send({msg: 'accounts'});
+})
+app.get('/transactions', (req, res)=>{    
+    res.send({msg: 'transactions'});
+})
 app.get('/', (req, res)=>{
     res.send('hello world!!');
+})
+// middleware , handler error msg
+app.use( (error, req, res, next)=>{
+    res.status(500).send(`msg: ${error}`)
 })
 
 console.log(`listening port ${port} ...`);
